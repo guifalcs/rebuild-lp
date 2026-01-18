1b3a5c5566a1a5de0118e1fa8bdef4140d02ec52
Guilherme Falcão <guifalcao2017@gmail.com>
2026-01-18 17:55:38 -0300

    Imagens em webp

--- src/app/sections/solutions/solutions.component.ts
+++ src/app/sections/solutions/solutions.component.ts
@@ -25,7 +25,7 @@ export class SolutionsComponent {
       tag: 'HEALTH',
       title: 'Agente virtual de agendamentos para clínicas',
       description: 'Desenvolvimento de agentes de agendamento inteligente e independente. Utilizando IA conversacional, o agente interage com pacientes para marcar, remarcar ou cancelar consultas, integrando-se diretamente com sistemas de gestão clínica. O projeto inclui reconhecimento de linguagem natural para entender solicitações complexas e fornecer respostas contextuais.',
-      image: '/saude.jpg'
+      image: '/saude.webp'
     },
     {
       id: 'financeiro',
@@ -33,7 +33,7 @@ export class SolutionsComponent {
       tag: 'FINANCE',
       title: 'Régua de cobrança automatizada',
       description: 'Implementação de fluxos financeiros autônomos que monitoram o "contas a receber" em tempo real. O sistema manda diariamente mensagens personalizadas, adaptando o tom e a abordagem conforme o perfil do cliente. A solução integra-se com sistemas ERP para atualização automática do status de pagamento.',
-      image: '/financeiro.jpg'
+      image: '/financeiro.webp'
     },
     {
       id: 'recursosHumanos',
@@ -41,7 +41,7 @@ export class SolutionsComponent {
       tag: 'HUMAN RESOURCES',
       title: 'Triagem de currículos com IA',
       description: 'Criação de um agente especializado em recrutamento que automatiza a triagem inicial de currículos. Utilizando processamento de linguagem natural, o agente avalia candidatos com base em critérios pré-definidos, classificando-os e gerando rankings de melhores candidatos.',
-      image: '/rh.jpg'
+      image: '/rh.webp'
     },
     {
       id: 'comercial',
@@ -49,7 +49,7 @@ export class SolutionsComponent {
       tag: 'SALES',
       title: 'Agente para funil de vendas',
       description: 'Desenvolvimento de um agente de vendas que interage com leads em múltiplos canais, qualificando-os e nutrindo-os ao longo do funil de vendas. O agente utiliza técnicas de IA para personalizar comunicações, agendar reuniões e fornecer informações relevantes sobre produtos ou serviços, integrando-se com CRM para atualização automática dos dados dos leads.',
-      image: '/comercial.jpg'
+      image: '/comercial.webp'
     }
   ];
 
